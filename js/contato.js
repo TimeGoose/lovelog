f27de2c6771f99ca9b0b8f502da23792469ed397
Camila <camila0830@gmail.com>
2019-02-11 14:49:56 -0200

    alterado arquivo bd

--- js/contato.js
+++ js/contato.js
@@ -1,37 +1,54 @@
-// Initialize Firebase
-var LeadRef = firebase.database().ref('Leads');
+var config = {
+          apiKey: "AIzaSyC-bvrk1HJ3OsBXMn-Xp6jda5CiWIHkCCE",
+          authDomain: "lovelog-code.firebaseapp.com",
+          databaseURL: "https://lovelog-code.firebaseio.com",
+          projectId: "lovelog-code",
+          storageBucket: "lovelog-code.appspot.com",
+          messagingSenderId: "952561236834"
+        };
+        
+        firebase.initializeApp(config);
 
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
 
+// Listen for form submit
+document.getElementById('formLead').addEventListener('submit', submitForm);
 
-//Ref form
+// Submit form
+function submitForm(e){
+  e.preventDefault();
 
-document.getElementById('formLead').addEventListener('submit', enviaForm);
+  // Get values
+  var name = getInputVal('nome');
+  var email = getInputVal('email');
+  
 
-function enviaForm(e){
-	e.preventDefault();
-	
-	var nome = obterDados('nome');
-	var email = obterDados('email');
+  // Save message
+  saveMessage(nome, email);
 
-	salvarLead(email, nome);
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('formLead').reset();
 }
 
-//Obter dados
-
-function obterDados(id){
-	return document.getElementById(id).value;
-
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
 }
 
-//Persistir no Firebase
-
-function salvarLead(email, nome){
-
-	var newLeadRef = LeadRef.push();
-	newLeadRef.set({
-
-		nome: nome,
-		email: email,
-	});
-
+// Save message to firebase
+function saveMessage(name, email){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    email:email,
+  });
 }
