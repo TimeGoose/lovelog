34c10302eb51aaa7ffc3a0310777ec9790dc6361
Gustavo Lopes <gustavo_lsilva@hotmail.com>
2019-02-12 00:39:12 -0200

    implantando conexao com Firebase

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDrp9ANTbTmOCk7NcLXAnJdErPw7JIR2II",
+    authDomain: "lovelogform.firebaseapp.com",
+    databaseURL: "https://lovelogform.firebaseio.com",
+    projectId: "lovelogform",
+    storageBucket: "lovelogform.appspot.com",
+    messagingSenderId: "690036997366"
+  };
+  firebase.initializeApp(config);
+
+  //reference msg collect
+  var messagesRef = firebase.database().ref('messages');
+
+
+  //form submit
+  document.getElementById('formAssinatura').addEventListener('submit', submitForm);
+
+  function submitForm(e){
+      e.preventDefault();
+
+    //get values
+    let name = getInputVal('name');
+    let email = getInputVal('email');
+    let car = getInputVal('car');
+
+    //save message
+    saveMessage(name, email, car);
+  }
+
+  //func para pegar os valores
+  function getInputVal(id){
+      return document.getElementById(id).value;
+  }
+
+  //save msg to firebase
+  function saveMessage(name, email, car){
+      var newMessageRef = messagesRef.push()
+
+      newMessageRef.set({
+          name: name,
+          email: email,
+          car: car,
+      });
+  } 
